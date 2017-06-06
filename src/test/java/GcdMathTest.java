1e25f23bdc422da674bfdbcc6e8c92b51ff023fd
Tomasz Sułecki <t.sulecki95@gmail.com>
2017-06-06 15:39:30 +0200

    gcd

--- src/test/java/GcdMathTest.java
+++ src/test/java/GcdMathTest.java
@@ -1,51 +1,85 @@
 import com.google.common.math.IntMath;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class GcdMathTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
-    public void bothArgumentAreNotNegative(){
-        //setup
+    public void bothArgumentAreNotNegative() {
+        //Arrange
         int a = 125;
         int b = 25;
-        //examine
-        int gcd = IntMath.gcd(a,b);
-        //verify
+        //Act
+        int gcd = IntMath.gcd(a, b);
+        //Assert
         assertEquals(25, gcd);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void firstArgumentIsNotNegativeAndSecondIsNegative(){
-        //setup
+    @Test
+    public void firstArgumentIsNotNegativeAndSecondIsNegative() {
+        //Arrange
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("b (-1) must be >= 0");
         int a = 25;
         int b = -1;
-        //examine
-        int gcd = IntMath.gcd(a,b);
-        //verify
+        //Act
+        int gcd = IntMath.gcd(a, b);
+        //Assert
         fail();
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void bothArgumentsAreNegative(){
-        //setup
+    @Test
+    public void bothArgumentsAreNegative() {
+        //Arrange
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("a (-1) must be >= 0");
         int a = -1;
         int b = -1;
-        //examine
-        int gcd = IntMath.gcd(a,b);
-        //verify
+        //Act
+        int gcd = IntMath.gcd(a, b);
+        //Assert
         fail();
     }
 
-    @Test(expected = IllegalArgumentException.class )
-    public void firstArgumentIsNegativeAndSecondIsNotNegative(){
-        //setup
+    @Test
+    public void firstArgumentIsNegativeAndSecondIsNotNegative() {
+        //Arrange
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("a (-1) must be >= 0");
         int a = -1;
         int b = 2;
-        //examine
-        int gcd = IntMath.gcd(a,b);
-        //verify
+        //Act
+        int gcd = IntMath.gcd(a, b);
+        //Assert
         fail();
+    }
+
+    @Test
+    public void resultForTwoZerosShouldBeZero() {
+        //Arrange
+        int a = 0;
+        int b = 0;
+        //Act
+        int gcd = IntMath.gcd(a, b);
+        //Assert
+        assertEquals(0, gcd);
+    }
+
+    @Test
+    public void resultIsCorrect() {
+        //Arrange
+        int a = 2670;
+        int b = 2015;
+        //Act
+        int gcd = IntMath.gcd(a, b);
+        //Assert
+        assertEquals(5, gcd);
     }
 }
