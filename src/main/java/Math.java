23694da03cffe93c104f9bc28508bb2801bc36f5
bartlomey <bartx67@gmail.com>
2017-06-06 14:10:49 +0200

    Testy pow

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -1,10 +1,12 @@
+import com.google.common.math.IntMath;
+
 import static com.google.common.math.IntMath.binomial;
 
 public class Math {
 
     public static void main(String[] args) {
 
-        System.out.println(binomial(-5, -2));
+        System.out.println(IntMath.pow(-5, -2));
 
     }
 }
--- src/test/java/PowMathTest.java
+++ src/test/java/PowMathTest.java
@@ -1,3 +1,40 @@
+import com.google.common.math.IntMath;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-public class PowMathTest {
+import static com.google.common.math.IntMath.binomial;
+import static com.google.common.math.IntMath.pow;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class PowMathTest
+{
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void PowShouldReturnCorrectValue()
+    {
+        //Arrange
+        //Act
+        int result = pow(2,4);
+
+        //Assert
+        assertEquals(16,result);
+    }
+
+    @Test
+    public void binomialShouldThrowExceptionWhenNSmallerThanK()
+    {
+        //Arrange
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("exponent (-2) must be >= 0");
+
+        //Act
+        pow(5,-2);
+
+        //Assert
+        fail();
+    }
 }
