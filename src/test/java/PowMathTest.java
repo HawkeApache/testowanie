6960481a62f7cd8485a68bac2b8774c4b534777e
AdrianLato <adrian.lato@student.uj.edu.pl>
2017-06-06 17:01:38 +0200

    Add test reports

--- src/test/java/PowMathTest.java
+++ src/test/java/PowMathTest.java
@@ -1,78 +1,68 @@
-import com.google.common.math.IntMath;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static com.google.common.math.IntMath.binomial;
 import static com.google.common.math.IntMath.pow;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-public class PowMathTest
-{
+public class PowMathTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void PowShouldThrowException()
-    {
+    public void PowShouldThrowException() {
         //Arrange
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("exponent (-2) must be >= 0");
 
         //Act
-        pow(5,-2);
+        pow(5, -2);
 
         //Assert
         fail();
     }
 
     @Test
-    public void PowShouldAlsoThrowException()
-    {
+    public void PowShouldAlsoThrowException() {
         //Arrange
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("exponent (-2) must be >= 0");
 
         //Act
-        pow(-5,-2);
+        pow(-5, -2);
 
         //Assert
         fail();
     }
 
     @Test
-    public void PowShouldReturnCorrectValue()
-    {
+    public void PowShouldReturnCorrectValue() {
         //Arrange
         //Act
-        int result = pow(2,4);
+        int result = pow(2, 4);
 
         //Assert
-        assertEquals(16,result);
+        assertEquals(16, result);
     }
 
     @Test
-    public void PowShouldReturnProperValue()
-    {
+    public void PowShouldReturnProperValue() {
         //Arrange
         //Act
-        int result = pow(-2,4);
+        int result = pow(-2, 4);
 
         //Assert
-        assertEquals(16,result);
+        assertEquals(16, result);
     }
 
     @Test
-    public void PowShouldReturnOne()
-    {
+    public void PowShouldReturnOne() {
         //Arrange
         //Act
-        int result = pow(0,0);
+        int result = pow(0, 0);
 
         //Assert
-        assertEquals(1,result);
+        assertEquals(1, result);
     }
-
-
 }
