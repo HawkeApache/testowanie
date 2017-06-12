74b0a221d584764e917d1ba19b2c3e5d0467ccaa
AdrianLato <adrian.lato@student.uj.edu.pl>
2017-06-12 16:36:03 +0200

    generate new reports

--- src/test/java/BinomialPerformanceTest.java
+++ src/test/java/BinomialPerformanceTest.java
@@ -9,10 +9,10 @@ public class BinomialPerformanceTest {
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         List<TimeStorage> storages = new LinkedList<>();
         TimeStorage timeStorage;
-        for (int i = 0; i <= 100000; i= i+100) {
+        for (int i = 20; i <= 100020; i= i+100) {
             timeStorage = new TimeStorage();
             try (Timer timer = new Timer(timeStorage)) {
-                int a = binomial(i, 0);
+                int a = binomial(i, 20);
             } catch (Exception e) {
                 e.printStackTrace();
             }
